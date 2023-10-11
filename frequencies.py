1896e2aa280de752e414b02a12e0fcecaa592c89
nuriashafqat <146842113+nuriashafqat@users.noreply.github.com>
2023-10-11 16:49:08 +0100

    Correct spelling of 'frequences'

--- frequencies.py
+++ frequencies.py
@@ -6,10 +6,8 @@ def frequencies(items):
     for item in items:
         key = str(item)
         if key in frequencies:
-            frequences[key] +=1
+            frequencies[key] +=1
         else:
             frequencies[key] = 1
     return frequencies
 
-list = [1, 2, "Hello", 2]
-frequencies(list)
